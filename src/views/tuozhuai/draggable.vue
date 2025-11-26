bca9a8355f27b49899c1cde5fedb5ee619c163b1
Sun123day <2478496130@qq.com>
2025-11-26 14:33:19 +0800

    feat:提交vuedraggable作为拖拽组件的操作

---
+++ src/views/tuozhuai/draggable.vue
@@ -0,0 +1,54 @@
+<template>
+  <div>
+    <draggable
+      :list="list"
+      animation="300"
+      ghost-class="ghost"
+      chosen-class="chosenClass"
+      @start="onStart"
+      @end="onEnd"
+
+      class="list-group"
+    >
+      <template #item="{ element }">
+        <div class="list-group-item">
+          {{ element.name }}
+        </div>
+      </template>
+    </draggable>
+  </div>
+</template>
+
+<script setup>
+import draggable from 'vuedraggable'
+import { ref } from 'vue'
+const list = ref([
+        { name: "John", id: 0 },
+        { name: "Joao", id: 1 },
+        { name: "Jean", id: 2 }
+      ])
+const onStart = () => {
+
+}
+const onEnd = () => {
+
+}
+</script>
+
+<style lang="scss" scoped>
+.ghost {
+  opacity: 0.5;
+  background: #c8ebfb;
+}
+.list-group {
+  display: flex;
+  flex-direction: column;
+  gap: 8px;
+}
+.list-group-item {
+  width: 160px;
+  height: 48px;
+  background-color: #898787;
+  cursor: pointer;
+}
+</style>
--- src/views/tuozhuai/index.vue
+++ src/views/tuozhuai/index.vue
@@ -1,11 +1,14 @@
 <script setup>
 import quanbu from './quanbu.vue'
 import fanwei from './fanwei.vue'
+import draggable from './draggable.vue'
 </script>
 
 <template>
   <div>
-    <quanbu />
-    <fanwei />
+    <!-- <quanbu />
+    <fanwei /> -->
+    <!-- 上面这两个都别用 -->
+    <draggable />
   </div>
 </template>
