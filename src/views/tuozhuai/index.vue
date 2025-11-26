0506daa2f842ecb92c0c6bcc6ddb23a065f69750
Sun123day <2478496130@qq.com>
2025-11-26 14:45:23 +0800

    revert:删除拖拽中不适用的内容

--- src/views/tuozhuai/index.vue
+++ src/views/tuozhuai/index.vue
@@ -1,14 +1,9 @@
 <script setup>
-import quanbu from './quanbu.vue'
-import fanwei from './fanwei.vue'
 import draggable from './draggable.vue'
 </script>
 
 <template>
   <div>
-    <!-- <quanbu />
-    <fanwei /> -->
-    <!-- 上面这两个都别用 -->
     <draggable />
   </div>
 </template>
